0769748b0801747496eccbf67624089ccbe7b878
Michel Zarpelon <michelzarpe@gmail.com>
2020-08-26 09:53:32 -0300

    h2 config, create primery test

--- src/test/java/com/wallet/HelloWord.java
+++ src/test/java/com/wallet/HelloWord.java
@@ -1,5 +1,7 @@
 package com.wallet;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,10 +11,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
-public class WalletApplicationTests {
+public class HelloWord {
 
 	@Test
-	public void contextLoads() {
+	public void testHelloWord() {
+		assertEquals(1, 1);
 	}
-
 }
